e274a9e4b49701909075afc24769a0cbf9bf1af2
Sergey <pankratozzi@gmail.com>
2021-07-06 01:27:30 +0300

    python file modified

--- project_alpha/fill_movie_title.py
+++ project_alpha/fill_movie_title.py
@@ -9,11 +9,11 @@ import time
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-r', '--rows', default=300, help='number of rows to insert 1..432')
+parser.add_argument('-r', '--rows', default=300, help='number of rows to insert 1..576')
 args = vars(parser.parse_args())
 num_rows = int(args['rows'])
 
-my_list = [(x, y, j, i) for (x, y, j, i) in product(range(1, 201, 35), range(1, 41, 12),
+my_list = [(x, y, j, i) for (x, y, j, i) in product(range(1, 351, 45), range(1, 41, 12),
                                                             range(1, 81, 15), range(1, 20, 7))]
 shuffle(my_list)
 # print(*my_list[:5], len(my_list))
@@ -27,11 +27,14 @@ cursor = conn.cursor()
 cursor.execute('SELECT id FROM movie_title ORDER BY id DESC LIMIT 1')
 last_id = cursor.fetchall()[0][0]
 for i in tqdm(range(num_rows), total=num_rows):
-    cursor.execute('INSERT INTO movie_title (id, media_id, producer_id, actor_id, studio_id)'
-                   'VALUES (%s, %s, %s, %s, %s)', ((last_id + i + 1), my_list[i][0], my_list[i][1],
-                                           my_list[i][2], my_list[i][3]))
+    try:
+        cursor.execute('INSERT INTO movie_title (id, media_id, producer_id, actor_id, studio_id)'
+                       'VALUES (%s, %s, %s, %s, %s)', ((last_id + i + 1), my_list[i][0], my_list[i][1],
+                                                       my_list[i][2], my_list[i][3]))
+    except mysql.connector.Error as err:
+        sys.stderr.write(f'{err.errno}: {err.msg}')
     time.sleep(0.05)
 cursor.execute('SELECT COUNT(*) FROM movie_title')
-sys.stderr.write(str(cursor.fetchall()[0][0]) + 'rows total')
+sys.stderr.write(str(cursor.fetchall()[0][0]) + ' rows total')
 conn.commit()
 conn.close()
